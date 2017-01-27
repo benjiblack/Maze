674ae06c30ed1afa887a9d99bd1d78c0a7166ca3
Ben Black <24kben@gmail.com>
2017-01-27 13:26:33 -0600

    Updated README.md

--- src/main/Java/Main.java
+++ src/main/Java/Main.java
@@ -25,7 +25,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-
+        System.out.println("Maze Runner Started");
         while (gameActive){
             int gameType = JOptionPane.showConfirmDialog(
                     null,
